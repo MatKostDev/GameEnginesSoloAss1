88467867cc7080bd8af0aeb2df24a855cc872b34
MatK <mathew.kostrzewa@ontariotechu.net>
2020-10-28 11:58:41 -0400

    did the assignment

---
+++ Assets/_Scripts/BulletPoolManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: Bonus - make this class a Singleton!
+
+[System.Serializable]
+public class BulletPoolManager : MonoBehaviour
+{
+    public GameObject bullet;
+    public int numInitialBullets;
+
+    Queue<GameObject> m_bulletQueue = new Queue<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BuildBulletPool();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject GetBullet()
+    {
+        GameObject myBullet;
+
+        if (IsPoolEmpty())
+        {
+            myBullet = SpawnBullet(true);
+        }
+        else
+        {
+            myBullet = m_bulletQueue.Dequeue();
+            GetPoolSize();
+            myBullet.SetActive(true);
+        }
+
+        return myBullet;
+    }
+    
+    public void ResetBullet(GameObject bullet)
+    {
+        bullet.SetActive(false);
+
+        m_bulletQueue.Enqueue(bullet);
+    }
+
+    public bool IsPoolEmpty()
+    {
+        return m_bulletQueue.Count <= 0;
+    }
+
+    public int GetPoolSize()
+    {
+        Debug.Log(m_bulletQueue.Count);
+        return m_bulletQueue.Count;
+    }
+
+    GameObject SpawnBullet(bool a_activate)
+    {
+        GetPoolSize();
+
+        GameObject newBullet = Instantiate(bullet, transform);
+        newBullet.GetComponent<BulletController>().bulletPool = this;
+
+        newBullet.SetActive(a_activate);
+
+        return newBullet;
+    }
+
+    void BuildBulletPool()
+    {
+        for (int i = 0; i < numInitialBullets; i++)
+        {
+            m_bulletQueue.Enqueue(SpawnBullet(false));
+        }
+    }
+}
